9f792934b751fd92e866e46450ecaef1296ce74f
danzel-py <67574872+danzel-py@users.noreply.github.com>
2021-07-11 22:47:14 +0900

    local db connected

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 'use strict';
 require('dotenv').config();
 const express     = require('express');
-const bodyParser  = require('body-parser');
 const cors        = require('cors');
+const mongoose    = require('mongoose')
+
 
 const apiRoutes         = require('./routes/api.js');
 const fccTestingRoutes  = require('./routes/fcctesting.js');
@@ -14,18 +15,9 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.use(cors({origin: '*'})); //For FCC testing purposes only
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-//Sample front-end
-app.route('/b/:board/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/board.html');
-  });
-app.route('/b/:board/:threadid')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/thread.html');
-  });
 
 //Index page (static HTML)
 app.route('/')
@@ -33,19 +25,41 @@ app.route('/')
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
-//For FCC testing purposes
-fccTestingRoutes(app);
+//Connect to Database
 
-//Routing for API 
-apiRoutes(app);
+const url = 'mongodb://127.0.0.1:27017/AMB'
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true});
 
-//404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('connected')
+  
+  //Sample front-end
+  app.route('/b/:board/')
+    .get(function (req, res) {
+      res.sendFile(process.cwd() + '/views/board.html');
+    });
+  app.route('/b/:board/:threadid')
+    .get(function (req, res) {
+      res.sendFile(process.cwd() + '/views/thread.html');
+    });
+  
+  
+  //For FCC testing purposes
+  fccTestingRoutes(app);
+  
+  //Routing for API 
+  apiRoutes(app);
+  
+  //404 Not Found Middleware
+  app.use(function(req, res, next) {
+    res.status(404)
+      .type('text')
+      .send('Not Found');
+  });
+
 });
-
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port " + process.env.PORT);
