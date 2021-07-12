920d5c48802c2c8417db9a54cd7ddfdb61ada265
danzel-py <67574872+danzel-py@users.noreply.github.com>
2021-07-12 15:17:16 +0900

    feat: get,post replie

--- routes/api.js
+++ routes/api.js
@@ -35,23 +35,23 @@ module.exports = function (app) {
   app.route('/api/threads/:board')
     .get((req, res) => {
       const boardName = req.params.board
-      console.log("Get board "+req.params.board)
+      console.log("Get board " + req.params.board)
       Board.findOne({ name: boardName }, 'posts', (err, board) => {
         if (err) return console.log("Error: Get Board")
         if (!board) return res.send([])
         let arr = board.posts
         arr.sort((a, b) => {
           // Sort by newest
-          return ( b.created_on - a.created_on)
+          return (b.bumped_on - a.bumped_on)
         })
 
-        let b = arr.slice(0,10).map((e)=>{
+        let b = arr.slice(0, 10).map((e) => {
           return {
             "_id": e._id,
             "text": e.text,
             "created_on": e.created_on,
             "bumped_on": e.bumped_on,
-            "replies": e.replies,
+            "replies": e.replies.slice(e.replies.length-3),
             "reply_count": e.replies.length
           }
         })
@@ -62,11 +62,11 @@ module.exports = function (app) {
     })
     .post((req, res) => {
       const boardName = req.params.board
-      console.log("Post board:"+ boardName+ " "+req.body.text)
+      console.log("Post board:" + boardName + " " + req.body.text)
       Board.findOneAndUpdate(
         { name: boardName },
         { $set: { name: boardName } },
-        { upsert: true , new: true},
+        { upsert: true, new: true },
         // ^ find or create Board
         (err, board) => {
           if (err) return res.send(err)
@@ -76,11 +76,10 @@ module.exports = function (app) {
             replies: []
           }))
           board.save()
-          res.redirect('/b/'+req.params.board+'/')
+          res.redirect('/b/' + req.params.board + '/')
         }
       )
     })
-
     .put((req, res) => {
       console.log("Put board (report)")
       const idReport = req.body.report_id
@@ -90,11 +89,13 @@ module.exports = function (app) {
           "name": boardName,
           "posts._id": idReport
         },
-        {"$set":{
-          "posts.$.reported": true
-        }},{new : true},
-        (err,done)=>{
-          if(err) return console.log(err)
+        {
+          "$set": {
+            "posts.$.reported": true
+          }
+        }, { new: true },
+        (err, done) => {
+          if (err) return console.log(err)
           res.send("Reported")
         }
       )
@@ -113,15 +114,15 @@ module.exports = function (app) {
         },
         {
           "$pull": {
-            "posts":{
+            "posts": {
               "_id": threadId,
               "delete_password": pw
             }
           }
         },
-        { new : true},
-        (err,done)=>{
-          if(err) return console.log(err)
+        { new: true },
+        (err, done) => {
+          if (err) return console.log(err)
           res.send("Success")
         }
 
@@ -129,15 +130,71 @@ module.exports = function (app) {
     })
 
 
-    // * REPLIES
+  // * REPLIES
   app.route('/api/replies/:board')
     .get((req, res) => {
-      console.log(req.body)
+      // params board
+      // body none
+      // query thread_id
+      Board.findOne(
+        {
+          "name": req.params.board,
+          "posts._id": req.query.thread_id
+        },
+        'posts',
+        (err, board) => {
+          if (err) return console.log(err)
+          if (!board) return console.log("no board")
+          let e = board.posts[0]
+          let aaaa = {
+            _id: e._id,
+            text: e.text,
+            created_on: e.created_on,
+            bumped_on: e.bumped_on,
+            replies: e.replies.map((el) => {
+              return {
+                _id: el._id,
+                text: el.text,
+                created_on: el.created_on
+              }
+            })
+          }
+          res.send(aaaa)
+        }
+      )
+
     })
     .post((req, res) => {
       console.log(req.body)
       // board, thread_id, text, delete_password
+      const boardName = req.params.board
+      const threadId = req.body.thread_id
 
+      Board.findOneAndUpdate(
+        {
+          "name": boardName,
+          "posts._id": threadId
+        },
+        {
+          $push: {
+            "posts.$.replies":
+              new Reply({
+                text: req.body.text,
+                delete_password: req.body.delete_password
+              })
+          },
+          $set:{
+            "posts.$.bumped_on": new Date()
+          }
+        },
+        {
+          new: true
+        },
+        (err, done) => {
+          if (err) return console.log(err)
+          console.log(done)
+        }
+      )
     })
     .put((req, res) => {
       console.log(req.body)
