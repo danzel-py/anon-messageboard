055adc8606aee21ba75eda8f6f2897a657a8ac17
danzel-py <67574872+danzel-py@users.noreply.github.com>
2021-07-16 20:31:27 +0900

    feat: 10 test cases

--- tests/functional-tests.js
+++ tests/functional-tests.js
@@ -90,10 +90,157 @@ suite('Functional Tests', function() {
                                 delete_password: newThread2.incorrect_delete_password
                             })
                             .end((err,res)=>{
-                                chai.assert(res.text,"incorrect password")
+                                assert.equal(res.text,"incorrect password")
                                 done()
                             })
                     })
             })
     })
+    test("Deleting a thread with the correct password: DELETE request to /api/threads/{board} with a valid delete_password",(done)=>{
+        chai.request(server)
+            .get("/api/threads/"+testBoard.name)
+            .end((err,res)=>{
+                let id = res.body[1]._id
+                chai.request(server)
+                    .delete("/api/threads/" + testBoard.name)
+                    .send({
+                        thread_id: id,
+                        delete_password: newThread2.delete_password
+                    })
+                    .end((err,res)=>{
+                        assert.equal(res.text,"success")
+                        done()
+                    })
+            })
+    })
+    test("Reporting a thread: PUT request to /api/threads/{board}",(done)=>{
+        chai.request(server)
+            .get("/api/threads/"+testBoard.name)
+            .end((err,res)=>{
+                let id = res.body[0]._id
+                chai.request(server)
+                    .put("/api/threads/" + testBoard.name)
+                    .send({
+                        report_id: id
+                    })
+                    .end((err,res)=>{
+                        assert.equal(res.text, "Reported")
+                        done()
+                    })
+            })
+            
+    })
+    test("Creating a new reply: POST request to /api/replies/{board}",(done)=>{
+        chai.request(server)
+            .get("/api/threads/" + testBoard.name)
+            .end((err,res)=>{
+                let id = res.body[0]._id
+                chai.request(server)
+                    .post("/api/replies/" + testBoard.name)
+                    .send({
+                        text: newReply.text,
+                        delete_password: newReply.delete_password,
+                        thread_id: id
+                    })
+                    .redirects(0)
+                    .end((err,res)=>{
+                        assert.equal(res.status, 302)
+                        done()
+                    })
+            })
+    })
+    test("Viewing a single thread with all replies: GET request to /api/replies/{board}",(done)=>{
+        chai.request(server)
+            .get("/api/threads/" + testBoard.name)
+            .end((err,res)=>{
+                let id = res.body[0]._id
+                chai.request(server)
+                    .get("/api/replies/"+testBoard.name+"?thread_id="+id)
+                    .end((err,res) =>{
+                        assert.isArray(res.body.replies)
+                        assert.equal(res.body.replies[0].text, newReply.text)
+                        done()
+                    })
+            })
+    })
+    test("Deleting a reply with the incorrect password: DELETE request to /api/replies/{board} with an invalid delete_password",(done)=>{
+        chai.request(server)
+            .get("/api/threads/" + testBoard.name)
+            .end((err,res)=>{
+                let id = res.body[0]._id
+                chai.request(server)
+                    .get("/api/replies/" + testBoard.name+"?thread_id="+id)
+                    .end((err,res)=>{
+                        let localThreadId = res.body.replies[0]._id
+                        chai.request(server)
+                            .delete("/api/replies/" + testBoard.name)
+                            .send({
+                                thread_id: id,
+                                reply_id: localThreadId,
+                                delete_password: "isThisReal??"
+                            })
+                            .end((err,res)=>{
+                                assert.equal(res.text,"no reply/ wrong pw")
+                                done()
+                            })
+                    })
+            })
+    })
+    test("Deleting a reply with the correct password: DELETE request to /api/replies/{board} with a valid delete_password",(done)=>{
+        chai.request(server)
+            .get("/api/threads/" + testBoard.name)
+            .end((err,res)=>{
+                let id = res.body[0]._id
+                chai.request(server)
+                    .get("/api/replies/" + testBoard.name+"?thread_id="+id)
+                    .end((err,res)=>{
+                        let repId = res.body.replies[0]._id
+                        chai.request(server)
+                            .delete("/api/replies/" + testBoard.name)
+                            .send({
+                                thread_id: id,
+                                reply_id: repId,
+                                delete_password: newReply.delete_password
+                            })
+                            .end((err,res)=>{
+                                assert.equal(res.status, 200)
+                                done()
+                            })
+                    })
+            })
+    })
+    test("Reporting a reply: PUT request to /api/replies/{board}",(done)=>{
+        chai.request(server)
+            .get("/api/threads/" + testBoard.name)
+            .end((err,res)=>{
+                let id = res.body[0]._id
+                chai.request(server)
+                    .post("/api/replies/" + testBoard.name)
+                    .send({
+                        text: newReply.text,
+                        delete_password: newReply.delete_password,
+                        thread_id: id
+                    })
+                    .redirects(0)
+                    .end((err,res)=>{
+                        assert.equal(res.status, 302)
+                        chai.request(server)
+                            .get("/api/replies/" + testBoard.name+"?thread_id="+id)
+                            .end((err,res)=>{
+                                let repId = res.body.replies[0]._id
+                                chai.request(server)
+                                    .put("/api/replies/" + testBoard.name)
+                                    .send({
+                                        thread_id: id,
+                                        reply_id: repId
+                                    })
+                                    .end((err,res)=>{
+                                        assert.equal(res.text, "Reported")
+                                        done()
+                                    })
+                            })
+                    })
+            })
+    })
 });
+// Reporting a reply: PUT request to /api/replies/{board}
